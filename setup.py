59450882a76ecdd6a228115aee3dcabb4274d8bf
Marek Čermák <prace.mcermak@gmail.com>
2019-10-20 16:07:57 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+import release_bot_test
+
+setuptools.setup(
+    name="release_bot_test_JLPy5otkl1EJi7XV",
+    version=release_bot_test.__version__,
+    author="User Cont",
+    author_email="user-cont@redhat.com",
+    description="A small example package used fo release-bot testing",
+    long_description="Test",
+    long_description_content_type="text/markdown",
+    url="https://github.com/CermakM/release-bot-test-JLPy5otkl1EJi7XV",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
